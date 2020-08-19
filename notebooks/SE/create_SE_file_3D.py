377e77fb1376f73cf800b60a2ef07fd09d915e48
fedorsidorov <fedor.sidorov.92@yandex.ru>
2020-08-19 16:55:34 +0300

    SE code for 3D structure is here

--- notebooks/SE/create_SE_file_3D.py
+++ notebooks/SE/create_SE_file_3D.py
@@ -6,8 +6,8 @@ from mpl_toolkits.mplot3d import Axes3D
 # %% read datafile
 l_x = 10
 l_y = 10
-nx = 20 + 1
-ny = 20 + 1
+nx = 50 + 1
+ny = 50 + 1
 
 xx = np.linspace(-l_x / 2, l_x / 2, nx)
 yy = np.linspace(-l_y / 2, l_y / 2, ny)
@@ -15,7 +15,7 @@ zz = np.zeros((len(xx), len(yy)))
 
 for i in range(len(xx)):
     for j in range(len(yy)):
-        zz[i, j] = np.sin(xx[i] * yy[j]) + 1
+        zz[i, j] = np.sin(xx[i] * yy[j]) + 2
 
 plt.figure(dpi=300)
 plt.imshow(zz)
@@ -25,12 +25,12 @@ plt.show()
 n_vertices = nx * ny * 2
 VV = np.zeros((n_vertices, 1 + 3))
 VV_inv = np.zeros((2, nx, ny), dtype=int)
-cnt = 0
+cnt = 1
 
 for i, x in enumerate(xx):
     for j, y in enumerate(yy):
-        VV[cnt, :] = cnt, x, y, 0
-        VV[nx * ny + cnt, :] = nx * ny + cnt, x, y, zz[i, j]
+        VV[cnt - 1, :] = cnt, x, y, 0
+        VV[nx * ny + cnt - 1, :] = nx * ny + cnt, x, y, zz[i, j]
         VV_inv[0, i, j] = cnt
         VV_inv[1, i, j] = nx * ny + cnt
         cnt += 1
@@ -42,45 +42,162 @@ for i, x in enumerate(xx):
 
 # %% edges
 n_edges = ((nx - 1) * ny + (ny - 1) * nx) * 2 + nx * 2 + ny * 2 - 4
-EE = np.zeros((n_edges, 1 + 2))
+EE = np.zeros((n_edges, 1 + 2), dtype=int)
+VV_EE = np.zeros((2, nx, ny, 6), dtype=int)
 
-fig = plt.figure(dpi=300)
-ax = fig.add_subplot(111, projection='3d')  # sdf
-cnt = 0
+# fig = plt.figure(dpi=300)
+# ax = fig.add_subplot(111, projection='3d')  # sdf
+cnt = 1
 
-for i in range(nx-1):  # lower layer ^
+for i in range(nx - 1):  # lower layer >
     for j in range(ny):
-        EE[cnt, :] = cnt, VV_inv[0, i, j], VV_inv[0, i+1, j]
+        EE[cnt - 1, :] = cnt, VV_inv[0, i, j], VV_inv[0, i + 1, j]
+        VV_EE[0, i, j, 0] = cnt
+        VV_EE[0, i + 1, j, 2] = cnt
         cnt += 1
 
-for i in range(nx):  # lower layer >
-    for j in range(ny-1):
-        EE[cnt, :] = cnt, VV_inv[0, i, j], VV_inv[0, i, j+1]
+for i in range(nx):  # lower layer ^
+    for j in range(ny - 1):
+        EE[cnt - 1, :] = cnt, VV_inv[0, i, j], VV_inv[0, i, j + 1]
+        VV_EE[0, i, j, 1] = cnt
+        VV_EE[0, i, j + 1, 3] = cnt
         cnt += 1
 
-for i in range(nx-1):  # upper layer ^
+for i in range(nx - 1):  # lower layer >
     for j in range(ny):
-        EE[cnt, :] = cnt, VV_inv[1, i, j], VV_inv[1, i+1, j]
+        EE[cnt - 1, :] = cnt, VV_inv[1, i, j], VV_inv[1, i + 1, j]
+        VV_EE[1, i, j, 0] = cnt
+        VV_EE[1, i + 1, j, 2] = cnt
         cnt += 1
 
-for i in range(nx):  # upper layer >
+for i in range(nx):  # lower layer ^
+    for j in range(ny - 1):
+        EE[cnt-1, :] = cnt, VV_inv[1, i, j], VV_inv[1, i, j + 1]
+        VV_EE[1, i, j, 1] = cnt
+        VV_EE[1, i, j + 1, 3] = cnt
+        cnt += 1
+
+for i in range(nx):  # lower-upper
+    for j in range(ny):
+        if not ((i == 0 or j == 0) or (i == nx - 1 or j == ny - 1)):
+            continue
+        EE[cnt - 1, :] = cnt, VV_inv[0, i, j], VV_inv[1, i, j]
+        VV_EE[0, i, j, 4] = cnt
+        VV_EE[1, i, j, 5] = cnt
+        cnt += 1
+
+# %% faces
+n_faces = (nx - 1) * (ny - 1) * 2 + (nx - 1) * 2 + (ny - 1) * 2
+FF = np.zeros((n_faces, 1 + 4), dtype=int)
+
+cnt = 1
+
+for i in range(nx - 1):  # bottom
+    for j in range(ny - 1):
+        FF[cnt-1, :] = cnt, VV_EE[0, i, j, 1], VV_EE[0, i, j + 1, 0],\
+                       -VV_EE[0, i + 1, j, 1], -VV_EE[0, i, j, 0]
+        cnt += 1
+
+for i in range(nx - 1):  # top
     for j in range(ny-1):
-        EE[cnt, :] = cnt, VV_inv[1, i, j], VV_inv[1, i, j+1]
+        FF[cnt-1, :] = cnt, VV_EE[1, i, j, 0], VV_EE[1, i + 1, j, 1],\
+                       -VV_EE[1, i, j + 1, 0], -VV_EE[1, i, j, 1]
         cnt += 1
 
-for i in range(nx):
-    EE[cnt, :] = cnt, VV_inv[0, i, 0], VV_inv[1, i, 0]
+for i in range(nx - 1):  # front
+    FF[cnt - 1, :] = cnt, VV_EE[0, i, 0, 0], VV_EE[0, i + 1, 0, 4],\
+                     -VV_EE[1, i + 1, 0, 2], -VV_EE[1, i, 0, 5]
     cnt += 1
 
-for i in range(nx):
-    EE[cnt, :] = cnt, VV_inv[0, i, ny-1], VV_inv[1, i, ny-1]
+for i in range(nx - 1):  # back
+    FF[cnt - 1, :] = cnt, VV_EE[0, i, ny - 1, 4], VV_EE[1, i, ny - 1, 0], \
+                     -VV_EE[1, i + 1, ny - 1, 5], -VV_EE[0, i + 1, ny - 1, 2]
     cnt += 1
 
-for j in range(1, ny-1):
-    EE[cnt, :] = cnt, VV_inv[0, 0, j], VV_inv[1, 0, j]
+for j in range(ny - 1):  # left
+    FF[cnt - 1, :] = cnt, VV_EE[0, 0, j, 4], VV_EE[1, 0, j, 1], \
+                     -VV_EE[1, 0, j + 1, 5], -VV_EE[0, 0, j + 1, 3]
     cnt += 1
 
-for j in range(1, ny-1):
-    EE[cnt, :] = cnt, VV_inv[0, nx-1, j], VV_inv[1, nx-1, j]
+for j in range(ny - 1):  # right
+    FF[cnt - 1, :] = cnt, VV_EE[0, nx - 1, j, 1], VV_EE[0, nx - 1, j + 1, 4], \
+                     -VV_EE[1, nx - 1, j + 1, 3], -VV_EE[1, nx - 1, j, 5]
     cnt += 1
+
+# %% prepare datafile
+file = ''
+file += 'define vertex attribute vmob real\n\n'
+
+file += 'MOBILITY_TENSOR \n' + \
+           'vmob  0     0\n' + \
+           '0     vmob  0\n' + \
+           '0     0  vmob\n\n'
+
+file += 'PARAMETER l_x = ' + str(l_x) + '\n'
+file += 'PARAMETER l_y = ' + str(l_y) + '\n'
+file += 'PARAMETER angle_surface = 55' + '\n'
+file += 'PARAMETER angle_mirror = 90' + '\n'
+
+file += 'PARAMETER TENS_r = 33.5e-2' + '\n'
+file += 'PARAMETER TENS_s = -TENS_r*cos((angle_surface_ind)*pi/180)' + '\n'
+file += 'PARAMETER TENS_w = -TENS_r*cos((angle_mirror_ind)*pi/180)' + '\n\n'
+
+file += '/*--------------------CONSTRAINTS START--------------------*/\n'
+file += 'constraint 1\n'
+file += 'formula: z = 0\n\n'
+
+file += 'constraint 11\n'
+file += 'formula: x = -l_x/2\n\n'
+
+file += 'constraint 22\n'
+file += 'formula: y = -l_y/2\n\n'
+
+file += 'constraint 33\n'
+file += 'formula: x = l_x/2\n\n'
+
+file += 'constraint 44\n'
+file += 'formula: y = l_y/2\n\n'
+
+file += '/*--------------------CONSTRAINTS END--------------------*/\n\n'
+
+# %% vertices
+file += '/*--------------------VERTICES START--------------------*/\n'
+file += 'vertices\n'
+
+for line in VV:
+    file += str(int(line[0])) + '\t' + str(line[1:])[1:-1] + '\n'
+    file += ' vmob 0\n'
+
+file += '/*--------------------VERTICES END--------------------*/\n\n'
+
+# %% edges
+file += '/*--------------------EDGES START--------------------*/\n'
+file += 'edges' + '\n'
+
+for line in EE:
+    file += str(line[0]) + '\t' + str(line[1:])[1:-1] + '\n'
+
+file += '/*--------------------EDGES END--------------------*/\n\n'
+
+# %% faces
+file += '/*--------------------FACES START--------------------*/\n'
+file += 'faces' + '\n'
+
+for i in range(len(FF)):
+    file += str(FF[i, 0]) + '\t' + str(FF[i, 1:])[1:-1] + '\n'
+
+file += '/*--------------------FACES END--------------------*/\n\n'
+
+# %% bodies
+file += '/*--------------------BODIES START--------------------*/\n'
+file += 'bodies' + '\n' + '1' + '\t'
+
+for fn in FF[:, 0]:
+    file += str(fn) + ' '
+
+file += '/*--------------------BODIES END--------------------*/\n\n'
+
+# %% write to file
+with open('notebooks/SE/SE_input_3D.fe', 'w') as myfile:
+    myfile.write(file)
 
